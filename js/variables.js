5122c31bb12dd4c9b397c9936dcc860e95e5265c
Tomas Pando <tomaspando11@gmail.com>
2024-10-02 18:28:04 -0300

    realizamos las tareas pedidas: multiplicacion, de Fahrenheit y perimetro y area

--- js/variables.js
+++ js/variables.js
@@ -6,3 +6,20 @@ let num2 = 23;
 const pi = 3.14;
 console.log("variable num1: " + num1);
 document.write("la suma de las variables es: " + (num1 + num2));
+
+//Multiplicar un número por 10:  Crear un algoritmo que permita al usuario ingresar un número y luego mostrar el resultado de multiplicarlo por 10.
+let num3 = 10;
+console.log("el resultado de multiplicarlo por 10 es: " + num3 * 10);
+document.write("<br>el resultado de multiplicarlo por 10 es " + num3 * 10);
+
+//Convertir grados Fahrenheit a Celsius: Crear un algoritmo que permita al usuario ingresar una temperatura en grados Fahrenheit y convertirla a grados Celsius. La fórmula para convertir de Fahrenheit a Celsius es: C= (5/9)​*(F−32)
+let num4 = 30;
+//console.log("la temperatura en grados es: " + (5/9)​*(num4-32));
+document.write("<br>la temperatura en grados es: " + ((num4 - 32) * 5) / 9);
+
+//Calcular el perímetro y área de un rectángulo: Crear un algoritmo que permita al usuario ingresar el largo y el ancho de un rectángulo, y calcular su perímetro y área. Las fórmulas son: Perímetro: P = 2*(largo+ancho) Área: A=largo×ancho
+let num5 = 10;
+let num6 = 2;
+document.write(
+  "<br>el perimetro es: " + 2 * (num5 + num6) + " y el area es: " + num5 * num6
+);

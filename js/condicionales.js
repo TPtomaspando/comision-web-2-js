32d7262300cd70454af9abb12e0874cd43451243
Tomas Pando <tomaspando11@gmail.com>
2024-10-03 11:28:36 -0300

    completamos las condiciones para el ejemplo de las elecciones

--- js/condicionales.js
+++ js/condicionales.js
@@ -1,7 +1,29 @@
-const numero = parseInt(prompt("ingrese numero"));
+//const numero = parseInt(prompt("ingrese numero"));
 
-if (numero % 2 === 0) {
-  document.write("el numero ingresado: " + numero + " es par");
+//if (numero % 2 === 0) {
+// document.write("el numero ingresado: " + numero + " es par");
+//} else {
+// document.write(`el numero ingresado: ${numero} es impar`);
+//}
+
+//ejercicio 2
+
+//Solicitar al usuario su edad e informar si esta obligado o no
+//16-17 o mayores de 70:puedes votar.(optativo)
+//18-70 : estas obligado a votar
+
+const edad = parseInt(prompt("ingrese su edad"));
+
+if (edad >= 1 && edad <= 105) {
+  if ((edad >= 16 && edad <= 17) || edad >= 70) {
+    document.write("es optativo que votes");
+  } else {
+    if (edad >= 18 && edad <= 70) {
+      document.write("es obligatorio que votes");
+    } else {
+      document.write("no puedes votar");
+    }
+  }
 } else {
-  document.write(`el numero ingresado: ${numero} es impar`);
+  alert("ingresaste un valor invalido");
 }

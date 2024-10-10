0c712c917c17fcaa820cb85d8f49e75a92d084b4
Tomas Pando <tomaspando11@gmail.com>
2024-10-10 10:10:00 -0300

    agregamos un ejemplo de while y do while

---
+++ js/4-bucles.js
@@ -0,0 +1,33 @@
+//while(condicion logica){
+//todas las lineas que yo quiera repetir
+//agregar una linea de codigo que haga que en algun momento la condicon no se cumpla en algun momento
+//}
+
+//crear un programa que muestre una lista de numeros del 1 al 100, cada uno en un renglon nuevo
+
+//let contador = 1;
+
+//while(contador <= 100){
+//  document.write("<p>Numero: "+contador+"</p>")
+//contador = contador + 1
+//}
+
+//do-while hacer-mientras
+
+/*
+do{
+todas las lineas de codigo que quiero repetir
+agregar una linea de codigo que haga que en algun momento la condicon no se cumpla en algun momento
+}while(condicion logica)
+
+
+
+
+*/
+
+//let numero = 1;
+
+//do {
+//document.write("<p>Numero: " + numero + "</p>");
+//numero++; //numero = numero +1
+//} while (numero <= 100);

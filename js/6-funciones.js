2dc4ca30fe65f76a89b0d3d3407f3df37db06a3b
Tomas Pando <tomaspando11@gmail.com>
2024-10-17 11:08:31 -0300

    agregamos ejemplo de funciones con parametro y sin parametro

---
+++ js/6-funciones.js
@@ -0,0 +1,32 @@
+//declarar variables
+const nombrePersona = prompt("ingrese un nombre");
+const apellidoPersona = prompt("ingrese un apellido");
+
+//funciones declarativas
+//funciones sin parametro
+function saludar() {
+  document.write("<p>Hola mundo 🌎</p>");
+}
+//funcion con parametro
+function saludarPersona(nombre, apellido) {
+  document.write(`<p>Hola ${nombre},${apellido}</p>`);
+}
+
+//funciones que retornan un valor
+function sumar(numero1, numero2) {
+  const total = numero1 + numero2;
+  return total;
+}
+
+//usar function
+saludar();
+
+document.write("<p>Aqui agrego un ejemplo de suma (4+2)</p>" + (4 + 2));
+document.write("<p>Aqui agrego un ejemplo de resta (4-2)</p>" + (4 - 2));
+document.write("<p>Aqui agrego un ejemplo de multiplicacion (4*2)</p>" + 4 * 2);
+
+saludarPersona(nombrePersona, apellidoPersona);
+saludarPersona("Christian", "Arrieta");
+
+const resultado = sumar(7, 20);
+document.write(`<p>El resultado de la suma es: ${resultado}</p>`);
